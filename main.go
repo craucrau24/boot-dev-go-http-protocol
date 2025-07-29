8db49a5ceb4355d7779a05d840d7ca6be6407552
Crawcraw <christophe.raucroy@gmail.com>
2025-07-29 11:59:02 +0200

    refactor read operation with channel

--- main.go
+++ main.go
@@ -8,29 +8,46 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lineCh := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(lineCh)
+
+		buf := make([]byte, 8)
+		var lineBuild strings.Builder
+
+		for {
+			count, err := f.Read(buf)
+			
+			if err == io.EOF {
+				break
+			}
+
+			lineBuild.Write(buf[:count])
+			lines := strings.Split(lineBuild.String(), "\n")
+			for i := range len(lines) - 1 {
+				lineCh <- lines[i]
+			}
+			lineBuild.Reset()
+			lineBuild.WriteString(lines[len(lines) - 1])
+		}
+
+		lineCh <- lineBuild.String()
+	}()
+
+	return lineCh
+}
+
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatalf("Cannot open file messages.txt: %v\n", err)
 	}
 
-	buf := make([]byte, 8)
-	var lineBuild strings.Builder
-
-	for {
-		count, err := file.Read(buf)
-		
-		if err == io.EOF {
-			break
-		}
-
-		lineBuild.Write(buf[:count])
-		lines := strings.Split(lineBuild.String(), "\n")
-		for i := range len(lines) - 1 {
-			fmt.Printf("read: %s\n", lines[i])
-		}
-		lineBuild.Reset()
-		lineBuild.WriteString(lines[len(lines) - 1])
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
-	fmt.Printf("read: %s\n", &lineBuild)
 }
