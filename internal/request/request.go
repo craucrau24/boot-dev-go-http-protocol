1ce772980dc82ca68a591949e9edc81999e22a9f
Crawcraw <christophe.raucroy@gmail.com>
2025-08-01 19:56:15 +0200

    read data by chunks instead of all at once

--- internal/request/request.go
+++ internal/request/request.go
@@ -3,17 +3,37 @@ package request
 import (
 	"fmt"
 	"io"
+	"slices"
 	"strings"
 )
 
+type ParseState int
+
+const (
+	StateInitialized ParseState = iota
+	StateDone
+)
+
 type Request struct {
-	RequestLine RequestLine
+	RequestLine *RequestLine
+	State ParseState
 }
 
 type RequestLine struct {
 	HttpVersion   string
 	RequestTarget string
 	Method        string
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	reqLine, count, err := parseRequestLine(data)
+	if count != 0 && err == nil {
+		r.RequestLine = reqLine
+		r.State = StateDone
+	} else if err != nil {
+		r.State = StateDone
+	}
+	return count, err
 }
 
 func splitRequestLine(line string) (string, string, string, error) {
@@ -24,16 +44,17 @@ func splitRequestLine(line string) (string, string, string, error) {
 	return parts[0], parts[1], parts[2], nil
 }
 
-func parseRequestLine(head []byte) (*RequestLine, error) {
+func parseRequestLine(head []byte) (*RequestLine, int, error) {
+	fmt.Println(head)
 	line, _, found := strings.Cut(string(head), "\r\n")
 	if !found {
-		return nil, fmt.Errorf("request line ill-formed: missing CRLF")
+		return nil, 0, nil
 	}
 
 
 	method, path, httpVer, err := splitRequestLine(line)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 
 	// Check method
@@ -45,29 +66,45 @@ func parseRequestLine(head []byte) (*RequestLine, error) {
 		}
 	}
 	if !allUpper {
-		return nil, fmt.Errorf("request method contains invalid characters")
+		return nil, 0, fmt.Errorf("request method contains invalid characters")
 	}
 
 	// Check http version
 	ver, found := strings.CutPrefix(httpVer, "HTTP/")
 	if !found || ver != "1.1" {
-		return nil, fmt.Errorf("request HTTP version not supported")
+		return nil, 0, fmt.Errorf("request HTTP version not supported")
 	}
 
-	return &RequestLine{Method: method, RequestTarget: path, HttpVersion: ver}, nil
+	return &RequestLine{Method: method, RequestTarget: path, HttpVersion: ver}, len(line) + 2, nil
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	req, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, fmt.Errorf("read error: %w", err)
+	req := Request {RequestLine: nil, State: StateInitialized}
+	data := make([]byte, 0)
+
+
+	buf := make([]byte, 8)
+	for req.State != StateDone {
+		read, err := reader.Read(buf)
+		if err != nil {
+			return &req, fmt.Errorf("error while reading from stream: %w", err)
+		}
+		data = slices.Concat(data, buf[:read])
+		parsed, err := req.parse(data)
+		if err != nil {
+			return &req, fmt.Errorf("error while parsing request: %w", err)
+		}
+		if parsed != 0 {
+			data = data[parsed:]
+		}
 	}
 
-	reqLine, err := parseRequestLine(req)
-	if err != nil {
-		return nil, err
+	for {
+		_, err := reader.Read(buf)
+		if err == io.EOF {
+			return &req, nil
+		} else if err != nil {
+			return &req, fmt.Errorf("error while reading from stream: %w", err)
+		}
 	}
-
-	return &Request{RequestLine: *reqLine}, nil
-
 }
